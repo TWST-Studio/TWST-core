47d291dd79fe8201899d54e4694faf4943c5be56
Maoyue <oneowo87@gmail.com>
2022-07-20 15:04:59 +0800

    update

--- src/main/java/studio/twst/core/Core.java
+++ src/main/java/studio/twst/core/Core.java
@@ -1,19 +1,20 @@
 package studio.twst.core;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("Plugin now enable");
+        Bukkit.getLogger().info("TWST core plugin now enable");
         // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        System.out.println("Plugin now disable");
+        Bukkit.getLogger().info("TWST core plugin now disable");
         // Plugin shutdown logic
     }
 }
