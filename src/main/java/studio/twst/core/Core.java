fc2992933529fc779e91dac070e990998f23fb96
Maoyue <oneowo87@gmail.com>
2022-07-20 13:21:35 +0800

    update

--- src/main/java/studio/twst/core/Core.java
+++ src/main/java/studio/twst/core/Core.java
@@ -6,12 +6,14 @@ public final class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        System.out.println("Plugin now on");
         // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
+        System.out.println("Plugin now off");
         // Plugin shutdown logic
     }
 }
